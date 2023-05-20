a87275c73edc6652ddec412d8fb2be3e07ceaebb
nikkhav <nikitakhavkin@gmail.com>
2023-05-20 15:46:35 +0200

    English translation

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,16 +1,29 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import { de } from "./assets/translations/translations";
+import { de, en } from "./assets/translations/translations";
+import { Platform, NativeModules } from 'react-native'
+
+const deviceLanguage =
+    Platform.OS === 'ios'
+        ? NativeModules.SettingsManager.settings.AppleLocale ||
+        NativeModules.SettingsManager.settings.AppleLanguages[0] //iOS 13
+        : NativeModules.I18nManager.localeIdentifier;
+
+const language = deviceLanguage.slice(0, 2)
 
 const resources = {
     de: {
         translation: de
+    },
+    en: {
+        translation: en
     }
 }
 
 i18next.use(initReactI18next).init({
     resources,
-    fallbackLng: 'de',
+    lng: language,
+    fallbackLng: 'en',
     interpolation: {
         escapeValue: false,
     },
